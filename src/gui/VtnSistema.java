f509c6a899b04737eae813c6e0df2896497a42e6
James <mendozajames02@hotmail.com>
2021-09-08 16:19:47 -0500

    Añadir barra menu a VtnSistema con Item crear usuario

--- src/gui/VtnSistema.java
+++ src/gui/VtnSistema.java
@@ -1,16 +1,22 @@
 package gui;
 
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 import modelo.Usuario;
 
-public class VtnSistema extends JFrame{
+public class VtnSistema extends JFrame {
 	private JPanel contentPane;
+	private JMenuBar menuBarra;
+	private JMenu menuCrear;
+	private JMenuItem menuCrearUsuario;
 	
 	public VtnSistema() {}
 	
-	public VtnSistema(Usuario u) {
+	public VtnSistema(Usuario usuarioLogeado) {
 		super("Sistema");
 		initComponents();
 	}
@@ -23,5 +29,19 @@ public class VtnSistema extends JFrame{
 		setLocationRelativeTo(null);
 		setResizable(false);
 		dispose();
+		
+		addMenu();
+	}
+	
+	private void addMenu() {
+		menuBarra = new JMenuBar();
+		
+		menuCrear = new JMenu("Crear");
+		menuCrearUsuario = new JMenuItem("Crear Usuario");
+		
+		menuCrear.add(menuCrearUsuario);
+		menuBarra.add(menuCrear);
+		
+		setJMenuBar(menuBarra);
 	}
 }
