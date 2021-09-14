4302106d10469d7b92b04e49a07a122b8f0cbbdc
James <mendozajames02@hotmail.com>
2021-09-14 18:33:18 -0500

    Centrar txtArea de vtnDocumento

--- src/gui/VtnDocumento.java
+++ src/gui/VtnDocumento.java
@@ -20,9 +20,11 @@ public class VtnDocumento extends JFrame{
 	private JPanel pnlInfoNorth;
 	private JPanel pnlInfoCenter;
 	private JPanel pnlInfoSouth;
+	
 	private JPanel pnlSouth;
 	private JPanel pnlSouthWest;
 	private JPanel pnlSouthEast;
+	
 	private JLabel lblTitulo;
 	private JLabel lblFecha;
 	private JTextArea txtAreaDescripcion;
@@ -82,17 +84,18 @@ public class VtnDocumento extends JFrame{
 	}
 	
 	private void initPnlInfoCenter() {
-		pnlInfoCenter = new JPanel();
+		pnlInfoCenter = new JPanel(new BorderLayout());
+		pnlInfoCenter.setBorder(new EmptyBorder(30, 0, 30, 0));
 		
 		txtAreaDescripcion = new JTextArea();
 
 		txtAreaDescripcion.setLineWrap(true);
 		txtAreaDescripcion.setWrapStyleWord(true);
-//		JScrollPane scrollTextArea = new JScrollPane(txtAreaDescripcion);
+		JScrollPane scrollTextArea = new JScrollPane(txtAreaDescripcion);
 		txtAreaDescripcion.setEditable(false);
 		txtAreaDescripcion.setText("Descripcion: Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
 		
-		pnlInfoCenter.add(txtAreaDescripcion);
+		pnlInfoCenter.add(scrollTextArea, BorderLayout.CENTER);
 		
 		pnlCenter.add(pnlInfoCenter, BorderLayout.CENTER);
 	}
